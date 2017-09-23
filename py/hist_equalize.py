0798a84d39f1973a92281b9e5668250394e5b91c
Evgeny <shtanov_evgenii@mail.ru>
2017-09-23 14:36:58 +0300

    Update hist equilize.
    Signed-off-by: Evgeny <shtanov_evgenii@mail.ru>

--- py/hist_equalize.py
+++ py/hist_equalize.py
@@ -9,7 +9,7 @@ hist,bins = np.histogram(img.flatten(), 256, [0,256])
 cdf = hist.cumsum()
 cdf_normalized = cdf * hist.max() / cdf.max()
 
-# Plot the results
+# Plot the input
 plt.subplot(2, 2, 1), plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(2, 2, 2), plt.plot(cdf_normalized, color = 'b')
@@ -18,12 +18,13 @@ plt.xlim([0,256])
 plt.legend(('cdf','histogram'), loc = 'upper left')
 plt.title('Histogram + CDF'), plt.xticks([]), plt.yticks([])
 
-
+# Equalization
 cdf_m = np.ma.masked_equal(cdf, 0)
 cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
 cdf = np.ma.filled(cdf_m, 0).astype('uint8')
 img2 = cdf[img]
 
+# Plot the results
 plt.subplot(2,2,3),plt.imshow(img2, cmap = 'gray')
 plt.title('Equalized image'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,2,4), plt.plot(cdf, color = 'b')
@@ -32,3 +33,9 @@ plt.xlim([0,256])
 plt.legend(('cdf','histogram'), loc = 'upper left')
 plt.title('Histogram + CDF'), plt.xticks([]), plt.yticks([])
 plt.show()
+
+# Histogram equalization by OpenCV
+img_grey = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
+equ = cv2.equalizeHist(img_grey)
+res = np.hstack((img_grey, equ)) #stacking images side-by-side
+cv2.imwrite('./../out/hist_eq_res.png', res)
