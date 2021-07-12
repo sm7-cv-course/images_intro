12986eec6f7d379b169e93b6c8a43ee94543d297
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2021-07-12 20:19:30 +0300

    Camera_calibration: sketch for points reading
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- py/camera_calibration.py
+++ py/camera_calibration.py
@@ -6,16 +6,33 @@ import glob
 
 # Number of intricics corners. Important: must be 1 odd and 1 even number!
 #bsize = (8, 5)
+#bsize = (7, 7)
 bsize = (7, 4)
 
+def read_points(fname):
+    points = []
+
+    # Using readlines()
+    file = open(fname, 'r')
+    Lines = file.readlines()
+
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        pts = line.split(',')
+        points.append([np.float32(pts[0]), np.float32(pts[1])])
+        #print("Line{}: {}".format(count, line.strip()))
+
+    return np.reshape(points, (len(points), 1, 2))
+
 ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--source", required=True, help="Path to source images.")
+ap.add_argument("-s", "--source", required=False, help="Path to source images.")
+ap.add_argument("-p", "--points", required=False, help="Path to precalculated corresponding points.")
 ap.add_argument("-i", "--image", required=False, help="Path to image to apply undistort methods.")
 args = vars(ap.parse_args())
 
 input_path = './../images/calibration'
-if args["source"] is not None:
-    input_path = args["source"]
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -27,33 +44,55 @@ objp[:,:2] = np.mgrid[0:bsize[0],0:bsize[1]].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob(input_path + '/*.jpg')
-images.extend(glob.glob(input_path + '/*.png'))
-images.extend(glob.glob(input_path + '/*.tiff'))
-images.extend(glob.glob(input_path + '/*.gif'))
-images.extend(glob.glob(input_path + '/*.jpeg'))
+if args["source"] is not None:
+    input_path = args["source"]
+    images = glob.glob(input_path + '/*.jpg')
+    images.extend(glob.glob(input_path + '/*.png'))
+    images.extend(glob.glob(input_path + '/*.tiff'))
+    images.extend(glob.glob(input_path + '/*.gif'))
+    images.extend(glob.glob(input_path + '/*.jpeg'))
+
+# Find corresponding points in images
+    for fname in images:
+        print(fname)
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        #cv.imshow('gray', gray)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, bsize, None)
+        print(ret)
+        # If found, add object points, image points (after refining them)
+        if ret:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners)
+            # Draw and display the corners
+            cv.drawChessboardCorners(img, bsize, corners2, ret)
+            #cv.imshow('img', img)
+            #key = cv.waitKey(500) & 0xFF
+            #if key == ord("q"):
+                #break
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    cv.destroyAllWindows()
 
 
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, bsize, None)
-    # If found, add object points, image points (after refining them)
-    if ret:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners)
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, bsize, corners2, ret)
-        cv.imshow('img', img)
-        key = cv.waitKey(500) & 0xFF
-        if key == ord("q"):
-            break
-cv.destroyAllWindows()
+# Load precalculated corresponding points
+if args["points"] is not None:
+    input_path = args["points"]
+    pts_files = glob.glob(input_path + '/*.txt')
+    for fname in pts_files:
+        print(fname)
+        points = read_points(fname)
+        if len(points) == bsize[0] * bsize[1]:
+            imgpoints.append(points)
+            objpoints.append(objp)
 
+    if args["image"] is not None:
+        input_image_path = args["image"]
+        img = cv.imread(input_image_path)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 if args["image"] is not None:
     input_image_path = args["image"]
